d1b7e6edec76cd2070bc5ba5236c00d96b26ccbd
shenry45 <shawn45henry@gmail.com>
2019-09-06 22:16:50 -0700

    User route for register returns jwt, login verifies jwt

--- api/server.js
+++ api/server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const helmet = require('helmet');
 const bcrypt = require('bcryptjs');
-// const jwt = require('jsonwebtokens');
+const jwt = require('jsonwebtoken');
 
 const DreamerRouter = require('../routes/dreamers/dreamer-routes');
 const InvestorRouter = require('../routes/investors/investor-routes');
@@ -32,11 +32,25 @@ server.post('/register', async (req, res) => {
       const addUser = await Users.addUser(hashUser);
 
       if (addUser) {
+        const tokenUser = {
+          id: addUser.id,
+          name: user.name,
+          email: user.email
+        }
+
+        const options = {
+          expiresIn: '3h'
+        }
+  
+        const token = jwt.sign(tokenUser, process.env.SECRET, options);
+
         res.status(201).json({
-          message: `Thank you ${user.name}. Your user had been created.`
+          message: `Thank you ${user.name}. Your user had been created.`,
+          token
         });
       }
     } else {
+
       res.status(400).json({
         message: 'All required fields not found'
       });
@@ -48,20 +62,29 @@ server.post('/register', async (req, res) => {
   }
 });
 
-// IN PROGRESS
-// server.get('/login', authenticate, async (req, res) => {
-//   const creds = req.body;
+// IN PROGRESS, needs to become middleware
+server.get('/login', async (req, res) => {
+  const creds = req.body;
+  const token = req.headers.token;
   
-//   try {
-//     if (creds.email && creds.password) {
-      
-//     }
-//   } catch(err) {
-//     res.status(500).json({
-//       error: err.message
-//     })
-//   }
-// });
+  try {
+    if (creds.email && creds.password) {
+      const verified = jwt.verify(token, process.env.SECRET);
+
+      if (verified) {
+        res.send('Logged In!');
+      } else {
+        res.status(403).json({
+          message: 'Invalid credentials'
+        })
+      }
+    }
+  } catch(err) {
+    res.status(500).json({
+      error: err.message
+    })
+  }
+});
 
 server.get('/projects', async (req, res) => {
   try {

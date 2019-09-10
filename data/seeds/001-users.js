9ecd09dabcb354bb6de5eb3e32f387fe472fa376
Cory Hale <corylhale2013@gmail.com>
2019-09-09 21:42:48 -0600

    Finished User Endpoints, Tested User Endpoints, Hashed user passwords, created working JSON Web Tokens.
    Co-authored-by: Shawn Henry <shawn45henry@gmail.com>

--- data/seeds/001-users.js
+++ data/seeds/001-users.js
@@ -7,14 +7,14 @@ exports.seed = function(knex) {
       return knex('users').insert([
         {
           name: 'John Doe',
-          password: 'test',
+          password: '$2a$10$wIzKGBzxZdLla5hm4/9PLOo/SBKyz/r/8XLAWLhQjwcGoCtTF21eu',
           email: 'test@test.com',
           location: 'Testing Facility',
           "type_id": 1
         },
         {
           name: 'Fon Doo',
-          password: 'test',
+          password: '$2a$10$wIzKGBzxZdLla5hm4/9PLOo/SBKyz/r/8XLAWLhQjwcGoCtTF21eu',
           email: 'fondont@food.com',
           location: 'Testing Facility',
           "type_id": 2
